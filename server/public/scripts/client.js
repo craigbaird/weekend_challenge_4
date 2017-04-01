033ea8162f1f45cf784fe6a44ef433a4ea43aef0
Craig Baird <bairdcraig@gmail.com>
2017-04-01 16:41:41 -0500

    separated listings

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,17 +1,56 @@
+// declare variables
+var forRentListings = [];
+var forSaleListings = [];
+
+// do this after document loads
 $(document).ready(function(){
-  init();
+  getListings();
 }); // end doc ready
 
-function init() {
-  getListings();
-}
-
+// GET all house listings
 function getListings() {
   $.ajax({
     type: "GET",
     url: "/listings",
     success: function(response){
-      console.log(response);
+    // console.log(response);
+    // appendListings(response);
+    checkStatus(response);
     }
   });
 }
+
+function checkStatus (responseToCheck){
+  for(var i = 0; i < responseToCheck.length; i ++){
+    if (responseToCheck[i].cost === undefined){
+      forRentListings.push(responseToCheck[i]);
+      // console.log(responseToCheck[i]);
+    } // end if
+    else {
+      forSaleListings.push(responseToCheck[i]);
+    }
+  } // end for
+  console.log(forRentListings);
+  console.log(forSaleListings);
+  appendListings(forRentListings, forSaleListings);
+}// end checkStatus
+
+function appendListings(forRentListings, forSaleListings) {
+  for(var i = 0; i < forRentListings.length; i ++){
+    var rentListing = forRentListings[i];
+    var $el = $(".listings").last();
+    $el.append("<li class ='list-group-item'>" + "<b>" + "Rent: " + "</b>" + "$" + rentListing.rent + "</li>");
+    $el.append("<li class ='list-group-item'>" + "<b>" + "SqFt: " + "</b>" + rentListing.sqft + "</li>");
+    $el.append("<li class ='list-group-item'>" + "<b>" + "City: " + "</b>" + rentListing.city + "</li>");
+    $el.append("<li class='spacer'>" + " " + "</li>");
+
+  } // end for
+  for(var j = 0; j < forSaleListings.length; j ++){
+    var saleListing = forSaleListings[j];
+    var $el2 = $(".listings").last();
+    $el2.append("<li class ='list-group-item'>" + "<b>" + "Cost: " + "</b>" + "$" + saleListing.cost + "</li>");
+    $el2.append("<li class ='list-group-item'>" + "<b>" + "SqFt: " + "</b>" + saleListing.sqft + "</li>");
+    $el2.append("<li class ='list-group-item'>" + "<b>" + "City: " + "</b>" + saleListing.city + "</li>");
+    $el2.append("<li class='spacer'>" + " " + "</li>");
+  } // end for
+} // end appendListings
--- server/routes/listings.js
+++ server/routes/listings.js
@@ -3,16 +3,17 @@ var router = express.Router();
 var mongoose = require("mongoose");
 
 var ListingsSchema = mongoose.Schema({
-rent : Number,
-sqFt: Number,
-city: String,
-});
+  "rent": Number,
+  "sqFt": Number,
+  "city": String,
+}); // end ListingsSchema
 
 var Listings = mongoose.model("Listings", ListingsSchema);
 
+// get all house listings from database and send to client.js
 router.get("/", function(req,res){
   //Get all listings
-  Listings.find(function(err, allListings){
+    Listings.find(function(err, allListings){
     if(err){
       console.log(err);
       res.sendStatus(500);
